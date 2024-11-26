33b310407e1024acbdd9d9bdff32deb297189272
Gustavo Musse <guprmusse@gmail.com>
2024-11-25 23:11:31 -0300

    Configuração do ´.editorconfig´

--- app/signin/page.tsx
+++
@@ -1,6 +0,0 @@
-const Signin = ()=>{
-
-return <h1> SignIn Page</h1>
-}
-
-export default Signin
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,6 +1,5 @@
 const Transactions = () => {
-
-    return <h1>Transactions Page</h1>
-}
+  return <h1>Transactions Page</h1>;
+};
 
 export default Transactions;

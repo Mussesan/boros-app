c53d74c02f8a9d99542b7a06c81a6dab478bd411
Gustavo Musse <guprmusse@gmail.com>
2024-11-25 23:33:35 -0300

    adiciona script `lint:check` e `lint:fix`

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
-const Login = ()=>{
+const Login = () => {
+  return <h1> Login Page</h1>;
+};
 
-return <h1> Login Page</h1>
-}
-
-export default Login
+export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
-const Home = () =>{
+const Home = () => {
+  return <h1 className="mx-auto w-max">Home Page!</h1>;
+};
 
-  return <h1 className="mx-auto w-max">Home Page!</h1>
-}
-
-export default Home
+export default Home;
